aa6bc91bff673d46c288c06246859844434105d1
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2014-03-04 16:29:51 +0000

    Correct issue with failing on first run
    Former-commit-id: 3fec4a330d4813765568eaa29618b1609a7f723c

--- get_isis_ip.bat
+++ get_isis_ip.bat
@@ -1,6 +1,8 @@
 @echo off
+set MYIP=127.0.0.1
 for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:"IPv4 Address" ^| findstr 130.246.`) do (
     set MYIP=%%f
-    set ISISIP=%MYIP: =%
-    goto :eof
+    goto :done
 )
+:done
+set ISISIP=%MYIP: =%
