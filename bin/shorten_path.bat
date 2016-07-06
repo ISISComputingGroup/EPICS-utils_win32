e98b4f5926137cfb8d77ea825eba0444296caf8a
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2016-07-06 17:25:26 +0100

    Add script to shorten windows PATHS

---
+++ bin/shorten_path.bat
@@ -0,0 +1,27 @@
+@echo off
+REM
+REM this will shorten the passed PATH variable by turning everything into its "DOS" name
+REM It will also remove folders that do not exist
+REM
+REM arg is a variable name like PATH
+REM
+if "%1" == "" (
+    @echo shorten_path: no path given
+    exit/b 1
+)
+SET "VarName=%%%~1%%"
+call set "OrigPath=%VarName%"
+
+setlocal EnableDelayedExpansion
+
+SET TempPath="%OrigPath:;=";"%"
+SET var=
+FOR %%a IN (%TempPath%) DO (
+    IF exist %%~sa (
+        SET "var=!var!;%%~sa"
+    ) ELSE (
+REM        echo %%a does not exist
+    )
+)
+
+endlocal & set "%~1=%var:~1%"
